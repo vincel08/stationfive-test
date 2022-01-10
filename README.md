3dc374e1aabe0b1f70ab5452fc71dd9dc43906a0
Alvin <alvinacuna55@gmail.com>
2022-01-10 23:41:36 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,16 @@
+#### install packages
+
+```
+yarn install
+```
+
+#### run the app
+
+```
+# default port is 3001
+yarn start
+
+or
+
+PORT=4000 yarn start
+```
--- backend-test/server.js
+++ backend-test/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { getResponse } = require('./lib/messaging')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(express.urlencoded({
